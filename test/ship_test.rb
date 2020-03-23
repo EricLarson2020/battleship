2acb92a659ade83f6c191a07ceed15c79f52f70f
Eric Larson <ericlarsonbroom@gmail.com>
2020-03-23 17:08:46 -0600

    wrote name and length tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,6 +8,16 @@ class ShipTest < Minitest::Test
     cruiser = Ship.new("Cruiser", 3)
     assert_instance_of Ship, cruiser
   end
+
+  def test_it_has_a_name
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "Cruiser", cruiser.name
+  end
+
+  def test_it_has_length
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, cruiser.length
+  end
 end
 # pry(main)> require './lib/ship'
 # #=> true
