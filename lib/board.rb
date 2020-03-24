e25e9dc6f1239663ebb5d9087bb16f5233a27ee1
Ross Perry <perryr16@gmail.com>
2020-03-23 18:29:55 -0600

    Initialize and cells methods built

---
+++ lib/board.rb
@@ -0,0 +1,27 @@
+class Board
+
+  def initialize
+    @x_size = 4
+    @y_size = 4
+  end
+
+  def cells
+    @cells = {"A1" => "cell",
+              "A2" => "cell",
+              "A3" => "cell",
+              "A4" => "cell",
+              "B1" => "cell",
+              "B2" => "cell",
+              "B3" => "cell",
+              "B4" => "cell",
+              "C1" => "cell",
+              "C2" => "cell",
+              "C3" => "cell",
+              "C4" => "cell",
+              "D1" => "cell",
+              "D2" => "cell",
+              "D3" => "cell",
+              "D4" => "cell"
+    }
+
+end #final
