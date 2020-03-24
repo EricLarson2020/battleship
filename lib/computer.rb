0090513552698760d7a99e179ca4293731d2258f
Ross Perry <perryr16@gmail.com>
2020-03-24 14:31:17 -0600

    computer cant pick if all cells are picked

--- lib/computer.rb
+++ lib/computer.rb
@@ -3,11 +3,12 @@ require "pry"
 class Computer
 
   attr_reader :board, :cell_list, :attack_cell
-  def initialize(board)
+  def initialize(board, ship1 = nil, ship2 = nil)
     @board = board
     @cell_list = @board.cells.keys
     @attack_cell
-
+    @ship1 = ship1
+    @ship2 = ship2
   end
 
   def attack
@@ -16,6 +17,8 @@ class Computer
       index = @cell_list.index(attack_cell)
       @cell_list.delete_at(index)
       @attack_cell
+    else
+      @attack_cell = nil
     end
   end
 
--- test/computer_test.rb
+++ test/computer_test.rb
@@ -16,9 +16,39 @@ class ComputerTest < Minitest::Test
   def test_it_picks_new_cells
     board = Board.new
     computer = Computer.new(board)
+    computer.attack
 
     assert_equal true, board.valid_coordinate?(computer.attack_cell)
   end
 
+  def test_it_cant_pick_if_cell_list_is_empty
+    board = Board.new
+    computer = Computer.new(board)
+    computer.attack # 1
+    assert_equal true, board.valid_coordinate?(computer.attack_cell)
+    computer.attack # 2
+    computer.attack # 3
+    computer.attack # 4
+    computer.attack # 5
+    computer.attack # 6
+    computer.attack # 7
+    computer.attack # 8
+    computer.attack # 9
+    computer.attack # 10
+    computer.attack # 11
+    computer.attack # 12
+    computer.attack # 13
+    computer.attack # 14
+    computer.attack # 15
+    assert_equal true, board.valid_coordinate?(computer.attack_cell)
+    computer.attack # 16
+    computer.attack # 17
+    #binding.pry
+    assert_equal nil, computer.attack_cell
 
+  end
+
+  def test_it_can_place_valid_ships
+    skip
+  end
 end
