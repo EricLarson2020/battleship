500c2a0d72c2369d1910029edc0ce245e0293d19
Ross Perry <perryr16@gmail.com>
2020-03-25 17:34:03 -0600

    adjacent cells built and tested

--- lib/computer.rb
+++ lib/computer.rb
@@ -5,25 +5,13 @@ class Computer
   attr_reader :board1, :cell_list, :attack_cell, :ship1, :ship2
   def initialize(board1, board2 = false)
     @board1 = board1
+    @board2 = board2
     @cell_list = @board1.cells.keys
     @attack_cell_list = @board1.cells.keys
     @place_cell_list = @board1.cells.keys
     @attack_cell = nil
     @ship1 = Ship.new("Cruiser", 3)
     @ship2 = Ship.new("Submarine", 2)
-  end
-
-  def attack
-    binding.pry
-    if !@attack_cell_list.empty?
-      @attack_cell = @attack_cell_list.shuffle[0]
-      index = @attack_cell_list.index(attack_cell)
-      @attack_cell_list.delete_at(index)
-      p @attack_cell_list
-      @attack_cell
-    else
-      @attack_cell = nil
-    end
   end
 
   def auto_coordinates(ship)
@@ -38,10 +26,49 @@ class Computer
     ship_cells
   end
 
+  def attack
+    # binding.pry
+    if !@attack_cell_list.empty?
+      @attack_cell = @attack_cell_list.shuffle[0]
+      index = @attack_cell_list.index(attack_cell)
+      @attack_cell_list.delete_at(index)
+      @attack_cell_list
+      @attack_cell
+    else
+      @attack_cell = nil
+    end
+  end
   # if status is hit
   #   attack adjacent
   #   if status is missed
   #     do random
+  def smart_attack
+    if @board2.cells[@attack_cell].status == :missed || @board2.cells[@attack_cell].status == :sunk
+      attack
+    elsif @board.cells[@attack_cell].status
+      #code
+    end
+  end
+
+  def adjacent_cells(cell)
+    letter = cell[0]
+    number = cell[1].to_i
+
+    num_minus = (number - 1).to_s
+    num_plus = (number + 1).to_s
+    letter_minus = (letter.ord - 1).chr
+    letter_plus = (letter.ord + 1).chr
+
+    adj1 = letter_minus + number.to_s
+    adj2 = letter_plus + number.to_s
+    adj3 = letter + num_plus
+    adj4 = letter + num_minus
+
+    p [adj1, adj2, adj3, adj4]
+
+
+  end
+
 
 
 end
