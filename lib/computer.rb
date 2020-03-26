e48ea4212d59067ec6f0aa6d47e7e73df9571aff
Ross Perry <perryr16@gmail.com>
2020-03-26 15:59:18 -0600

    refactor many larger methods

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,69 +2,104 @@ require "pry"
 
 class Computer
 
-  attr_reader :board1, :cell_list, :attack_cell, :ship1, :ship2
-  def initialize(board1, board2 = false)
-    @board1 = board1
-    @board2 = board2
-    @cell_list = @board1.cells.keys
-    @attack_cell_list = @board1.cells.keys
-    @place_cell_list = @board1.cells.keys
+  attr_reader :board_computer,
+              :cell_list,
+              :attack_cell,
+              :ship1,
+              :ship2,
+              :attack_cell_list
+  def initialize(board_computer, board_user = false)
+    @board_computer = board_computer
+    @board_user = board_user
+    @cell_list = @board_computer.cells.keys
+    @attack_cell_list = @board_computer.cells.keys
+    @place_cell_list = @board_computer.cells.keys
     @attack_cell = ''
     @ship1 = Ship.new("Cruiser", 3)
     @ship2 = Ship.new("Submarine", 2)
   end
 
+  def delete_cells(original, delete)
+    original.reject do |cell|
+      delete.include?(cell)
+    end
+  end
+
   def auto_coordinates(ship)
     ship_cells = []
-    until board1.valid_placement?(ship, ship_cells) do
+    until board_computer.valid_placement?(ship, ship_cells) do
       ship_cells = @place_cell_list.shuffle[0..(ship.length-1)]
     end
-    ship_cells.each do |cell|
-      index = @place_cell_list.index(cell)
-      @place_cell_list.delete(cell)
-    end
+    delete_cells(@place_cell_list, ship_cells)
     ship_cells
   end
 
   def attack
-    # binding.pry
-    # binding.pry
     if !@attack_cell_list.empty?
       @attack_cell = @attack_cell_list.shuffle[0]
-      index = @attack_cell_list.index(attack_cell)
-      @attack_cell_list.delete_at(index)
+      @attack_cell_list.delete(@attack_cell)
 
       @attack_cell
     else
       @attack_cell = ''
     end
-    @attack_cell
+    # binding.pry
   end
 
+  def missed_or_sunk?(previous_cell)
+    @board_user.cells[previous_cell].status == :missed || @board_user.cells[previous_cell].status == :sunk
+
+  end
+
+  def hit?(previous_cell)
+    @board_user.cells[previous_cell].status == :hit
+  end
+
+  def smart_cells(previous_cell)
+    adjacent_cells(previous_cell).find_all do |cell|
+      @attack_cell_list.include?(cell)
+
+    end
+  end
+
+  def smart_attack_cell(previous_cell)
+    if smart_cells(previous_cell) == []
+      attack
+    elsif smart_cells(previous_cell) != []
+      @attack_cell = smart_cells(previous_cell).shuffle[0]
+      @attack_cell_list.delete(@attack_cell)
+      puts "SMART ATTACK!"
+      @attack_cell
+    end
+  end
+
+
+
   def smart_attack
-    # binding.pry
     if @attack_cell == ''
       attack
-    elsif @board2.cells[@attack_cell].status == :missed || @board2.cells[@attack_cell].status == :sunk
+    elsif missed_or_sunk?(@attack_cell)
       puts "random attack"
       attack
-
-    elsif @board2.cells[@attack_cell].status == :hit
-      smart_cells = adjacent_cells(@attack_cell).find_all do |cell|
-        @attack_cell_list.include?(cell)
-      end
+    elsif hit?(@attack_cell)
       # binding.pry
-      if smart_cells == []
+      # smart_cell
+      # smart_cells = adjacent_cells(@attack_cell).find_all do |cell|
+      #   @attack_cell_list.include?(cell)
+      # end
+      # binding.pry
+
+      if smart_cells(@attack_cell) == []
         attack
-      else
-        @attack_cell = smart_cells.shuffle[0]
-        index = @attack_cell_list.index(@attack_cell)
-        @attack_cell_list.delete_at(index)
+      elsif smart_cells(@attack_cell) != []
+        @attack_cell = smart_cells(@attack_cell).shuffle[0]
+        @attack_cell_list.delete(@attack_cell)
         puts "SMART ATTACK!"
         @attack_cell
       end
     end
     # binding.pry
+
   end
 
   def adjacent_cells(cell)
@@ -82,6 +117,7 @@ class Computer
     adj4 = letter + num_minus
 
     adj_cells = [adj1, adj2, adj3, adj4]
+    #toss out invalid cells ( A - 1 = @ )
     adj_cells2 = adj_cells.reject do |cell|
       cell[1].to_i < 1 || cell[1].to_i > 4 || cell[0].ord < 65 || cell[0].ord > 68
     end
