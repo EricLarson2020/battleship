17830571df9d1a02386c3598d418e609970bd57a
Ross Perry <perryr16@gmail.com>
2020-03-26 17:12:12 -0600

    tests for player shot refactor

--- test/gameplay_test.rb
+++ test/gameplay_test.rb
@@ -4,8 +4,9 @@ require './lib/ship'
 require './lib/board'
 require './lib/cell'
 require './lib/gameplay'
+require './lib/computer'
 
-class ShipTest < Minitest::Test
+class GameplayTest < Minitest::Test
 
   def test_it_exists
     board1 = Board.new
@@ -15,25 +16,43 @@ class ShipTest < Minitest::Test
     assert_instance_of Gameplay, gameplay
   end
 
-  def test_game_over
+  def test_player_fires_on_cell
+    board_user = Board.new
+    board_computer = Board.new
+    computer = Computer.new(board_computer, board_user)
+    game = Gameplay.new(board_user, board_computer, computer)
 
-    board1 = Board.new
-    board2 = Board.new
-    computer = Computer.new(board2)
-    game = Gameplay.new(board1, board2, computer)
-    # submarine =Ship.new("submarine", 2)
-    # cruiser = Ship.new("cruiser", 3)
-    # game.welcome
-    # game.cruiser_assignment
-    # game.submarine_assignment
-    # cruiser = Ship.new("Cruiser", 1)
-    # board1.place(cruiser, ["A1"])
-    # board2.place(cruiser, ["B3"])
 
-    # game.start
+    game.player_fire_on("A1")
+    game.board_computer.cells["A1"].render
 
-    game.start
-    game.play_loop
+    assert_equal :missed, game.status_board_computer("A1")
+  end
+
+  def test_it_calls_players_shots_correctly
+    board_user = Board.new
+    board_computer = Board.new
+    computer = Computer.new(board_computer, board_user)
+    game = Gameplay.new(board_user, board_computer, computer)
+    cruiser = Ship.new("Cruiser", 3)
+    board_computer.place(cruiser, ["A1", "A2", "A3"])
+
+    game.player_fire_on("B1")
+    game.status_board_computer("B1")
+    expected = "Your shot on B1 was a miss."
+    assert_equal expected, game.player_call_result("B1")
+
+    game.player_fire_on("A1")
+    game.status_board_computer("A1")
+    expected = "Your shot on A1 was a hit!"
+    assert_equal expected, game.player_call_result("A1")
+
+    game.player_fire_on("A2")
+    game.player_fire_on("A3")
+    game.status_board_computer("A1")
+    expected = "You sunk computer's Cruiser!"
+    assert_equal expected, game.player_call_result("A1")
+
   end
 
 
