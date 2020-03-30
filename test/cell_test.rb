fdb13e6789ba1e6aada0d72628a087287cc18eda
Ross Perry <perryr16@gmail.com>
2020-03-30 14:39:45 -0600

    all methods tested

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -46,12 +46,14 @@ class ShipTest < Minitest::Test
   def test_cell_render_when_not_fired_upon
     cell_1 = Cell.new("B4")
     assert_equal ".", cell_1.render
+    assert_equal :not_hit, cell_1.status
   end
 
   def test_cell_render_when_fired_upon_and_missed
     cell_1 = Cell.new("B4")
     cell_1.fire_upon
     assert_equal "M", cell_1.render
+    assert_equal :missed, cell_1.status
   end
 
   def test_cell_render_when_fired_upon_and_ship_hit_and_ship_sunk
@@ -60,14 +62,18 @@ class ShipTest < Minitest::Test
     cruiser = Ship.new("Cruiser", 3)
     cell_2.place_ship(cruiser)
     assert_equal ".", cell_2.render
+    assert_equal :not_hit, cell_2.status
     # assert_equal "S", cell_2.render(true)
     cell_2.fire_upon
     assert_equal "H", cell_2.render
+    assert_equal :hit, cell_2.status
     assert_equal false, cruiser.sunk?
     cruiser.hit
     cruiser.hit
     assert_equal true, cruiser.sunk?
     assert_equal "X", cell_2.render
+    assert_equal :sunk, cell_2.status
+
   end
 end
 
